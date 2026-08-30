0f3d4db72cac518ec485c9693ab439ff2f5905da
kuramoto-to-the-moon <kuramoto.assets@gmail.com>
2026-08-31 06:42:55 +0900

    Bundle consistent UI fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource-variable/inter';
+import '@fontsource-variable/noto-sans-jp';
 import { App } from './App';
 import './styles.css';
 
